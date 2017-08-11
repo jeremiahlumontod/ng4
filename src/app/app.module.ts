bcbba6c904c85b1f0abde5e7b613fa624833fcf6
Jeremiah Lumontod <jeremiah.lumontod@gmail.com>
2017-08-12 04:43:46 +0800

    recipe ingredient maintenance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -16,30 +16,6 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-//import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     HeaderComponent,
-//     RecipesComponent,
-//     RecipeListComponent,
-//     RecipeDetailComponent,
-//     RecipeItemComponent,
-//     ShoppingListComponent,
-//     ShoppingEditComponent,
-//     DropdownDirective
-//   ],
-//   imports: [
-//     BrowserModule,
-//     FormsModule,
-//     HttpModule
-//   ],
-//   providers: [ShoppingListService],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
 
 @NgModule({
   declarations: [
@@ -58,6 +34,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule
   ],
